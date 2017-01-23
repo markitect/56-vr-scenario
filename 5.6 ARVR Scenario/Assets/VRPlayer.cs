ad27b053bbb9cef0237fa1a071324e6afc9f3fcd
William Jack <William Jack>
2017-01-23 14:16:32 -0800

    Added basic VRPlayer networked prefab

---
+++ 5.6 ARVR Scenario/Assets/VRPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.VR;
+
+public class VRPlayer : NetworkBehaviour {
+	void OnStartLocalPlayer()
+	{
+		GetComponent<Material>().color = Color.blue;
+
+		//test code
+		transform.position = new Vector3(Random.Range(-2,2), 1, Random.Range(-2, 2));
+		VRSettings.LoadDeviceByName("OpenVR");
+	}
+
+
+	void Update()
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+	}
+}
