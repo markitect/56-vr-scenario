31dbf4e66c0551df3e74d2ef7202dab3063af3d9
Ryan Hylland <Ryan Hylland>
2017-01-23 17:49:14 -0800

    LaserGun prefab and shooter script

---
+++ 5.6 ARVR Scenario/Assets/Scripts/LaserShooter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LaserShooter : MonoBehaviour
+{
+    public Color LaserColor;
+
+    [SerializeField] private GameObject m_LaserBeamPrefab;
+    [SerializeField] private Transform m_BarrelTipPosition;
+    [SerializeField] private float m_MaxBeamLength;
+    [SerializeField] private float m_MaxChargeTime;
+    [SerializeField] private ParticleSystem m_ChargingEffect;
+
+    private enum FireState
+    {
+        None,
+        Charging,
+        Firing
+    }
+    
+    private FireState m_FireState;
+    private GameObject m_Laserbeam;
+    private float m_BeamLength;
+    private LineRenderer m_Beam;
+    private float m_BeamSpeed;
+
+    void Update()
+    {
+        switch (m_FireState)
+        {
+            case FireState.None:
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    m_Laserbeam = Instantiate(m_LaserBeamPrefab);
+                    m_Beam = m_Laserbeam.GetComponent<LineRenderer>();
+                    m_Beam.numPositions = 2;
+                    m_Beam.GetComponent<Transform>().position = m_BarrelTipPosition.position;
+                    m_Beam.SetPosition(0, Vector3.zero);
+                    m_Beam.startColor = Color.blue;
+                    m_Beam.endColor = m_Beam.startColor;
+
+                    if (m_ChargingEffect != null)
+                        m_ChargingEffect.gameObject.SetActive(true);
+
+                    m_FireState = FireState.Charging;
+                }
+                break;
+
+            case FireState.Charging:
+                if (Input.GetButtonDown("Fire1") || m_BeamLength >= m_MaxBeamLength)
+                {
+                    m_Beam.SetPosition(1, new Vector3(0, 0, m_BeamLength));
+                    m_FireState = FireState.Firing;
+
+                    // set beam length back to zero for next beam creation.
+                    m_BeamLength = 0;
+                    break;
+                }
+
+                // this may have to change to better conform to the beam control system.
+                // but basically the beam should get longer the longer a player holds the button here.
+                m_BeamLength += m_MaxBeamLength * Time.deltaTime / m_MaxChargeTime;
+                break;
+
+            case FireState.Firing:
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    if (m_ChargingEffect != null)
+                        m_ChargingEffect.gameObject.SetActive(false);
+
+                    // TODO: fire event Mark is working which should be attached to the beam prefab.
+                    // should set the speed of the beam here too.
+                    Debug.Log("Fired laser!");
+                    m_FireState = FireState.None;
+                    break;
+                }
+
+                // increase or decrease beam speed?
+                m_BeamSpeed -= 5;
+                break;
+        }
+    }
+}
