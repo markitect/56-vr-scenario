f695dd09f21b83ac874a13186dfa9a0afb649f65
William Jack <williamj@unity3d.com>
2017-01-26 15:23:03 -0800

    Commenting out useless commands.

--- 5.6 ARVR Scenario/Assets/Scripts/Player.cs
+++ 5.6 ARVR Scenario/Assets/Scripts/Player.cs
@@ -7,11 +7,8 @@ using UnityEngine.VR;
 
 public class Player : NetworkBehaviour
 {
-
-    public GameObject ARPlayerInstance;
     public GameObject VRPlayerInstance;
 	public GameObject VRPlayerTools;
-
 
 	public Camera childCamera;
     public PlayerRoles role { get; private set; }
@@ -26,54 +23,12 @@ public class Player : NetworkBehaviour
 
 	void Start()
 	{
-        if (VRSettings.loadedDeviceName == "HoloLens")
-        {
-            CmdActivateArRig();
-            //RpcActivateArRig();
-            role = PlayerRoles.Blocker;
-            gameObject.AddComponent<ArControls>();
-        }
-        else
-        {
-            role = PlayerRoles.Shooter;
-        }
-
 		if (!isLocalPlayer)
 		{
 			Destroy(childCamera);
 			return;
 		}
 	}
-
-
-
-	[Command]
-    public void CmdActivateArRig()
-    {
-        // We to destroy VR camera to create a AR camera
-        Destroy(childCamera);
-
-		gameObject.GetComponent<ToolController>().enabled = false;
-
-		ARPlayerInstance.SetActive(true);
-        VRPlayerInstance.SetActive(false);
-		VRPlayerTools.SetActive(false);
-		netWorkTransfromChild.target = ARPlayerInstance.transform;
-    }
-
-    [ClientRpc]
-    public void RpcActivateArRig()
-    {
-        // We to destroy VR camera to create a AR camera
-        Destroy(childCamera);
-
-        gameObject.GetComponent<ToolController>().enabled = false;
-
-        ARPlayerInstance.SetActive(true);
-        VRPlayerInstance.SetActive(false);
-        VRPlayerTools.SetActive(false);
-        netWorkTransfromChild.target = ARPlayerInstance.transform;
-    }
 
     void Update()
 	{
