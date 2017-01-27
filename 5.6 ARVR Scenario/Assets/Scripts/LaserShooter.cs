ef3c4eca75c976f19d2d759d976eccc55d8af3dd
Mark Gehan <Mark Gehan>
2017-01-27 14:09:48 -0800

    fix for laser shooting and interactions with prisms

--- 5.6 ARVR Scenario/Assets/Scripts/LaserShooter.cs
+++ 5.6 ARVR Scenario/Assets/Scripts/LaserShooter.cs
@@ -7,140 +7,158 @@ using UnityEngine.Networking;
 
 public class LaserShooter : NetworkBehaviour
 {
-    private enum FireState
-    {
-        None,
-        Charging,
-        Firing
-    }
-    
-    private FireState m_FireState;
-    private GameObject m_Laserbeam;
-    private LineRenderer m_Beam;
-    private ShootLaser m_BeamScript;
-    private float m_BeamLength;
-    private float m_BeamSpeed;
-    
-    private bool b_CanChangeColor;
-    private int m_CurrentColorIndex;
-    private float m_ColorChangeTimer;
-    private Light m_ColorIndicator;
+	private enum FireState
+	{
+		None,
+		Charging,
+		Firing
+	}
+	
+	private FireState m_FireState;
+	private GameObject m_Laserbeam;
+	private LineRenderer m_Beam;
+	private ShootLaser m_BeamScript;
+	private float m_BeamLength;
+	private float m_BeamSpeed;
+	
+	private bool b_CanChangeColor;
+	private int m_CurrentColorIndex;
+	private float m_ColorChangeTimer;
+	private Light m_ColorIndicator;
 
 
-    [SerializeField] private GameObject m_LaserBeamPrefab;
-    [SerializeField] private Transform m_BarrelTipPosition;
+	[SerializeField] private GameObject m_LaserBeamPrefab;
+	[SerializeField] private Transform m_BarrelTipPosition;
 
-    [SerializeField] private float m_MaxBeamLength;
-    [SerializeField] private float m_MaxChargeTime;
-    [SerializeField] private float m_MaxBeamSpeed;
-    [SerializeField] private float m_MinBeamSpeed;
+	[SerializeField] private float m_MaxBeamLength;
+	[SerializeField] private float m_MaxChargeTime;
+	[SerializeField] private float m_MaxBeamSpeed;
+	[SerializeField] private float m_MinBeamSpeed;
 
-    [SerializeField] private ParticleSystem m_ChargingEffect;
-    [SerializeField] private Color[] m_AvailableColors;
-    [SerializeField] private float m_ColorChangeTimeLimit;
+	[SerializeField] private ParticleSystem m_ChargingEffect;
+	[SerializeField] private Color[] m_AvailableColors;
+	[SerializeField] private float m_ColorChangeTimeLimit;
+	[SerializeField] private LayerMask[] m_AvailableLaserLayers;
 
-    void Start()
-    {
-        b_CanChangeColor = true;
-        m_ColorIndicator = gameObject.GetComponentInChildren<Light>();
-    }
+	void Start()
+	{
+		b_CanChangeColor = true;
+		m_ColorIndicator = gameObject.GetComponentInChildren<Light>();
+	}
 
-    void Update()
-    {
-		if(!isLocalPlayer)
+	void Update()
+	{
+		if (!isLocalPlayer)
+		{
 			return;
+		}
 
-        m_ColorChangeTimer += Time.deltaTime;
+		m_ColorChangeTimer += Time.deltaTime;
 
-        switch (m_FireState)
-        {
-            case FireState.None:
-                if (Input.GetButtonDown("Fire1"))
-                {
-                    CmdFireLaser();
-                }
-                break;
+		switch (m_FireState)
+		{
+			case FireState.None:
+				if (Input.GetButtonDown("Fire1"))
+				{
+					CmdFireLaser();
+				}
+				break;
 
-            case FireState.Charging:
-                if (Input.GetButtonDown("Fire1") || m_BeamLength >= m_MaxBeamLength)
-                {
-                    m_BeamScript.length = m_BeamLength;
-                    m_FireState = FireState.Firing;
+			case FireState.Charging:
+				if (Input.GetButtonDown("Fire1") || m_BeamLength >= m_MaxBeamLength)
+				{
+					m_BeamScript.length = m_BeamLength;
+					m_FireState = FireState.Firing;
 
-                    // set beam length back to zero for next beam creation.
-                    m_BeamLength = 0;
-                    break;
-                }
+					// set beam length back to zero for next beam creation.
+					m_BeamLength = 0;
+					break;
+				}
 
-                m_BeamLength += m_MaxBeamLength * Time.deltaTime / m_MaxChargeTime;
-                break;
+				m_BeamLength += m_MaxBeamLength * Time.deltaTime / m_MaxChargeTime;
+				break;
 
-            case FireState.Firing:
-                if (Input.GetButtonDown("Fire1") || m_BeamSpeed <= m_MinBeamSpeed)
-                {
-                    if (m_ChargingEffect != null)
-                        m_ChargingEffect.gameObject.SetActive(false);
+			case FireState.Firing:
+				if (Input.GetButtonDown("Fire1") || m_BeamSpeed <= m_MinBeamSpeed)
+				{
+					if (m_ChargingEffect != null)
+						m_ChargingEffect.gameObject.SetActive(false);
 
-                    m_BeamScript.speed = m_BeamSpeed;
-                    m_Laserbeam.transform.position = m_BarrelTipPosition.position;
-                    m_Laserbeam.transform.rotation = m_BarrelTipPosition.rotation;
+					if (m_Laserbeam != null)
+					{
+						m_Laserbeam.transform.position = m_BarrelTipPosition.position;
+						m_Laserbeam.transform.rotation = m_BarrelTipPosition.rotation;
 
-                    m_BeamScript.FireLaser(this.gameObject);
-                    m_FireState = FireState.None;
-                    break;
-                }
+						m_BeamScript.FireLaser(
+							this.GetComponentInParent<ScoreKeeper>().gameObject,
+							this.m_BeamSpeed,
+							this.m_BeamLength,
+							this.m_AvailableLaserLayers[this.m_CurrentColorIndex],
+							this.m_AvailableColors[this.m_CurrentColorIndex]);
+					}
+					m_FireState = FireState.None;
+					break;
+				}
 
-                // increase or decrease beam speed?
-                m_BeamSpeed -= .1f;
-                break;
-        }
+				// increase or decrease beam speed?
+				m_BeamSpeed -= .1f;
+				break;
+		}
 
-        if (Input.GetAxis("Vertical") > .5 && b_CanChangeColor)
-        {
-            ChangeColor(1);
-        }
+		if (Input.GetAxis("Vertical") > .5 && b_CanChangeColor)
+		{
+			ChangeColor(1);
+		}
 
-        if (Input.GetAxis("Vertical") < -.5 && b_CanChangeColor)
-        {
-            ChangeColor(-1);
-        }
+		if (Input.GetAxis("Vertical") < -.5 && b_CanChangeColor)
+		{
+			ChangeColor(-1);
+		}
 
-        if (m_ColorChangeTimer > m_ColorChangeTimeLimit)
-            b_CanChangeColor = true;
+		if (m_ColorChangeTimer > m_ColorChangeTimeLimit)
+		{
+			b_CanChangeColor = true;
+		}
+	}
 
-    }
+	public void ChangeColor(int indexAmount)
+	{
+		m_CurrentColorIndex = (m_CurrentColorIndex + indexAmount) % m_AvailableColors.Length;
 
-    public void ChangeColor(int indexAmount)
-    {
-        m_CurrentColorIndex = (m_CurrentColorIndex + indexAmount) % m_AvailableColors.Length;
+		if (m_ColorIndicator != null)
+		{
+			m_ColorIndicator.color = m_AvailableColors[m_CurrentColorIndex];
+		}
 
-        if(m_ColorIndicator != null)
-            m_ColorIndicator.color = m_AvailableColors[m_CurrentColorIndex];
+		b_CanChangeColor = false;
+		m_ColorChangeTimer = 0;
+	}
 
-        b_CanChangeColor = false;
-        m_ColorChangeTimer = 0;
-    }
+	[Command]
+	public void CmdFireLaser()
+	{
+		m_Laserbeam = Instantiate(m_LaserBeamPrefab);
+		m_BeamScript = m_Laserbeam.GetComponent<ShootLaser>();
 
-    [Command]
-    public void CmdFireLaser()
-    {
-        m_Laserbeam = Instantiate(m_LaserBeamPrefab);
-        m_Beam = m_Laserbeam.GetComponent<LineRenderer>();
-        m_BeamScript = m_Laserbeam.GetComponent<ShootLaser>();
+		m_Laserbeam.transform.position = m_BarrelTipPosition.position;
+		m_Laserbeam.transform.rotation = m_BarrelTipPosition.rotation;
 
-        m_Laserbeam.transform.position = m_BarrelTipPosition.position;
-        m_Laserbeam.transform.rotation = m_BarrelTipPosition.rotation;
-        //m_BeamScript.laserColor = m_AvailableColors[m_CurrentColorIndex];
-        m_BeamSpeed = m_MaxBeamSpeed;
+		if (m_ChargingEffect != null)
+		{
+			m_ChargingEffect.gameObject.SetActive(true);
+		}
 
-        if (m_ChargingEffect != null)
-            m_ChargingEffect.gameObject.SetActive(true);
+		m_FireState = FireState.Charging;
 
-        m_FireState = FireState.Charging;
+		m_BeamScript.FireLaser(
+			this.GetComponentInParent<ScoreKeeper>().gameObject,
+			this.m_MinBeamSpeed,
+			this.m_MaxBeamLength,
+			this.m_AvailableLaserLayers[m_CurrentColorIndex],
+			this.m_AvailableColors[m_CurrentColorIndex]
+			);
 
-        m_Laserbeam.GetComponent<ShootLaser>().FireLaser(this.gameObject);
-
-        //Spawn on Server
-        NetworkServer.Spawn(m_Laserbeam);
-    }
+		//Spawn on Server
+		NetworkServer.Spawn(m_Laserbeam);
+	}
 }
--- 5.6 ARVR Scenario/Assets/Scripts/ShootLaser.cs
+++ 5.6 ARVR Scenario/Assets/Scripts/ShootLaser.cs
@@ -8,195 +8,196 @@ using UnityEngine.VR.WSA.Persistence;
 [RequireComponent(typeof(LineRenderer))]
 public class ShootLaser : MonoBehaviour
 {
-	public Color laserColor { get; set; }
-	public float speed { get; set; }
-	public float length { get; set; }
-	public bool isFiring { get; private set; }
+    public Color laserColor { get; set; }
+    public float speed { get; set; }
+    public float length { get; set; }
+    public bool isFiring { get; private set; }
+    public ScoreKeeper scorer { get; private set; }
 
-	public LayerMask collisionLayers;
-	public LayerMask laserLayerMask;
+    public LayerMask collisionLayers;
+    public LayerMask laserLayerMask;
 
-	private LineRenderer lineRenderer;
+    private LineRenderer lineRenderer;
 
-	private List<Vector3> linePoints = new List<Vector3>();
+    private List<Vector3> linePoints = new List<Vector3>();
 
-	private Vector3 start;
-	private Vector3 end;
-	private Vector3 direction;
-	private bool isScoring;
-	private ScoreKeeper scorer;
-	private float scoreTimer;
+    private Vector3 start;
+    private Vector3 end;
+    private Vector3 direction;
+    private bool isScoring;
+    private float scoreTimer;
 
-	private bool isDissolving;
+    private bool isDissolving;
 
-	private float laserDistance;
+    private float laserDistance;
 
-	private float startTime;
+    private float startTime;
 
-
-    // Use this for initialization
-    void Start()
+	void Awake()
 	{
 		this.lineRenderer = this.gameObject.GetComponent<LineRenderer>();
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.anyKey)
-		{
-			this.speed = 5f;
-			this.length = 10f;
-			this.laserLayerMask = LayerMask.NameToLayer("Red");
-			this.laserColor = Color.red;
-			this.FireLaser(this.gameObject);
-		}
+    // Use this for initialization
 
-		if (this.isFiring)
-		{
-			var step = Time.deltaTime * speed;
-			this.end += this.direction * step;
-			this.laserDistance += (this.direction * step).magnitude;
-			this.linePoints[this.linePoints.Count - 1] = end;
-			this.direction = Vector3.zero;
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isFiring)
+        {
+            var step = Time.deltaTime * speed;
+            this.end += this.direction * step;
+            this.laserDistance += (this.direction * step).magnitude;
+            this.linePoints[this.linePoints.Count - 1] = end;
+            this.direction = Vector3.zero;
+			
+			this.lineRenderer.numPositions = this.linePoints.Count;
+			this.lineRenderer.SetPositions(this.linePoints.ToArray());
 
 			RecalculatePoints();
 
-			if (laserDistance <= 30f)
-			{
-				RaycastForMovement();
-			}
+            if (this.laserDistance <= this.length)
+            {
+                RaycastForMovement();
+            }
 
-			if (this.laserDistance >= this.length)
-			{
-				isDissolving = true;
-			}
+            if (this.laserDistance >= this.length)
+            {
+                isDissolving = true;
+            }
 
-			if (isScoring)
-			{
-				scoreTimer += Time.deltaTime;
+            if (isScoring)
+            {
+                scoreTimer += Time.deltaTime;
 
-				if (scorer != null && scoreTimer >= scorer.Rate)
-				{
-					scorer.Score();
-				}
-			}
+                if (scorer != null && scoreTimer >= scorer.Rate)
+                {
+                    scorer.Score();
+                }
+            }
 
-			if (isDissolving)
-			{
-				// move the first point in the line to the 2nd point in the line by speed divided by the distance between the lines.
+            if (isDissolving)
+            {
+                // move the first point in the line to the 2nd point in the line by speed divided by the distance between the lines.
 
-				if (linePoints.Count > 1)
-				{
-					var dissolveStart = linePoints[0];
-					var lineFraction = speed * Time.deltaTime / Vector3.Distance(dissolveStart, linePoints[1]);
+                if (linePoints.Count > 1)
+                {
+                    var dissolveStart = linePoints[0];
+                    var lineFraction = speed * Time.deltaTime / Vector3.Distance(dissolveStart, linePoints[1]);
 
-					// if the line fraction is 1 then we have reached point 2.  Delete the first point to shorten the line.
-					if (lineFraction < 1)
-					{
-						linePoints[0] = Vector3.Lerp(dissolveStart, linePoints[1], lineFraction);
-					}
-					else
-					{
-						linePoints.RemoveAt(0);
-					}
-				}
+                    // if the line fraction is 1 then we have reached point 2.  Delete the first point to shorten the line.
+                    if (lineFraction < 1)
+                    {
+                        linePoints[0] = Vector3.Lerp(dissolveStart, linePoints[1], lineFraction);
+                    }
+                    else
+                    {
+                        linePoints.RemoveAt(0);
+                    }
+                }
 
-				// this objects job is done as there is only 1 point left on the line.
-				// this won't interfere with creation since it is regulated by the isDissolving flag.
-				if (linePoints.Count <= 1)
-				{
-					this.isFiring = false;
-					this.linePoints.Clear();
-					GameObject.Destroy(this.gameObject);
-				}
-			}
+                // this objects job is done as there is only 1 point left on the line.
+                // this won't interfere with creation since it is regulated by the isDissolving flag.
+                if (linePoints.Count <= 1)
+                {
+                    this.isFiring = false;
+                    this.linePoints.Clear();
+                    GameObject.Destroy(this.gameObject);
+                }
+            }
+        }
+    }
 
-			this.lineRenderer.numPositions = this.linePoints.Count;
-			this.lineRenderer.SetPositions(this.linePoints.ToArray());
-		}
-	}
+    public void FireLaser(GameObject owner, float speed, float distance, LayerMask laserLayerMask, Color color)
+    {
+        this.linePoints.Clear();
+        this.start = this.gameObject.transform.position;
+        this.linePoints.Add(start);
+        this.end = this.start;
+        this.linePoints.Add(end);
+        this.direction = this.transform.forward;
+        this.lineRenderer.startWidth = .05f;
+        this.lineRenderer.startColor = color;
+        this.lineRenderer.endColor = color;
+	    scorer = owner.GetComponent<ScoreKeeper>();
+        startTime = Time.time;
+	    this.speed = speed;
+	    this.length = distance;
+	    this.laserColor = color;
+        this.isFiring = true;
+        this.collisionLayers |= 1 << laserLayerMask.value;
+    }
 
-	public void FireLaser(GameObject owner)
-	{
-		this.linePoints.Clear();
-		this.start = this.gameObject.transform.position;
-		this.linePoints.Add(start);
-		this.end = this.start;
-		this.linePoints.Add(end);
-		this.direction = this.transform.forward;
-		this.lineRenderer.startWidth = .05f;
-		this.lineRenderer.startColor = this.laserColor;
-		this.lineRenderer.endColor = this.laserColor;
-		scorer = owner.GetComponent<ScoreKeeper>();
-		startTime = Time.time;
-		this.isFiring = true;
-		this.collisionLayers |= 1 << this.laserLayerMask.value;
-	}
+    private void RecalculatePoints()
+    {
+        var currentStart = this.linePoints.FirstOrDefault();
+        var currentEnd = this.linePoints.LastOrDefault();
+        var newDirection = (this.linePoints[1] - this.linePoints[0]).normalized;
+        this.linePoints.Clear();
+        this.linePoints.Add(currentStart);
+        var distanceTraveled = this.laserDistance;
 
-	private void RecalculatePoints()
-	{
-		var currentStart = this.linePoints.FirstOrDefault();
-		var currentEnd = this.linePoints.LastOrDefault();
-		var newDirection = (this.linePoints[1] - this.linePoints[0]).normalized;
-		this.linePoints.Clear();
-		this.linePoints.Add(currentStart);
-		var distanceTraveled = this.laserDistance;
-
-		for (var i = 0; i < this.lineRenderer.numPositions; ++i)
-		{
-			RaycastHit hit;
-			if (Physics.Raycast(new Ray(this.linePoints[i], newDirection), out hit, 1000f, this.collisionLayers) &&
-				(hit.point - this.linePoints[i]).magnitude < distanceTraveled)
-			{
-				var hitObject = hit.transform.gameObject;
-				this.linePoints.Add(hit.point);
-				distanceTraveled -= (hit.point - this.linePoints[i]).magnitude;
-				if (hitObject.layer == LayerMask.NameToLayer("Reflective"))
-				{
-					newDirection = Vector3.Reflect((hit.point - this.linePoints[i]).normalized, hit.normal);
-				}
-				else
-				{
-					return;
-				}
-			}
-			else if (this.laserDistance <= this.length)
-			{
-				this.linePoints.Add(this.linePoints.Last() + (newDirection*distanceTraveled));
-				return;
-			}
+        for (var i = 0; i < this.lineRenderer.numPositions; ++i)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(new Ray(this.linePoints[i], newDirection), out hit, 1000f, this.collisionLayers) &&
+                (hit.point - this.linePoints[i]).magnitude < distanceTraveled)
+            {
+                var hitObject = hit.transform.gameObject;
+                this.linePoints.Add(hit.point);
+                distanceTraveled -= (hit.point - this.linePoints[i]).magnitude;
+                if (hitObject.layer == LayerMask.NameToLayer("Reflective"))
+                {
+                    newDirection = Vector3.Reflect((hit.point - this.linePoints[i]).normalized, hit.normal);
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else if (this.laserDistance <= this.length)
+            {
+                this.linePoints.Add(this.linePoints.Last() + (newDirection*distanceTraveled));
+                return;
+            }
             else
-			{
-				this.linePoints.Add(currentEnd);
-				return;
-			}
-		}
-	}
+            {
+                this.linePoints.Add(currentEnd);
+                return;
+            }
+        }
+    }
 
-	private void RaycastForMovement()
-	{
-		RaycastHit hit;
-		if (Physics.Raycast(new Ray(this.start, (this.end - this.start).normalized), out hit, Vector3.Distance(start, end), this.collisionLayers))
-		{
-			switch (LayerMask.LayerToName(hit.collider.gameObject.layer))
-			{
-				case "Reflective":
-					var reflection = Vector3.Reflect(hit.point - this.start, hit.normal);
-					this.direction = reflection.normalized;
+    private void RaycastForMovement()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(new Ray(this.start, (this.end - this.start).normalized), out hit, Vector3.Distance(start, end), this.collisionLayers))
+        {
+            switch (LayerMask.LayerToName(hit.collider.gameObject.layer))
+            {
+                case "Reflective":
+                    var reflection = Vector3.Reflect(hit.point - this.start, hit.normal);
+                    this.direction = reflection.normalized;
+                    this.start = this.end;
+                    this.linePoints.Add(this.end);
+                    return;
+
+                case "ScoreTarget":
+                    isScoring = true;
+                    return;
+				case "Prism":
 					this.start = this.end;
-					this.linePoints.Add(this.end);
-					return;
-
-				case "ScoreTarget":
-					isScoring = true;
+					GameObject faceHit = hit.collider.gameObject;
+					faceHit.GetComponent<MeshRenderer>().material.color = laserColor;
+					GameObject parentPrism = faceHit.transform.parent.gameObject;
+					parentPrism.GetComponent<LaserSplitter>().SplitLaser(faceHit, this);
 					return;
 				default:
-					this.linePoints.Add(hit.point);
-					this.direction = Vector3.zero;
-					return;
-			}
-		}
-		this.direction = (this.end - this.start).normalized;
-	}
+                    this.linePoints.Add(hit.point);
+                    this.direction = Vector3.zero;
+                    return;
+            }
+        }
+        this.direction = (this.end - this.start).normalized;
+    }
 }
